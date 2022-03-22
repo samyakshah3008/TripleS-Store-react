38d3f52fd8e970939c15b7dc12b2ffd287ad219c
Samyak Shah <samyakshah3008@gmail.com>
2022-03-22 12:47:36 +0530

    REFACTOR: anchor tag removed

--- src/components/FeaturedProduct.jsx
+++ src/components/FeaturedProduct.jsx
@@ -14,9 +14,9 @@ export function FeaturedProduct() {
 
       <div className="product-container">
         <div className="product-card">
-          <a href="/styles/product/product.html" target="_blank">
+        
             <img src={mrfBat} className="card-img" alt="mrf-bat" />
-          </a>
+          
           <div className="card-info">MRF Cricket Bats</div>
           <i className="fas fa-star star selected-star"></i>
           <i className="fas fa-star star selected-star"></i>
@@ -26,9 +26,9 @@ export function FeaturedProduct() {
           <div className="product-price">$60.00</div>
         </div>
         <div className="product-card">
-          <a href="/styles/product/product.html" target="_blank">
+        
             <img src={nikeFootball} className="card-img" alt="football-nike" />
-          </a>
+        
           <div className="card-info">Nike Football</div>
           <i className="fas fa-star star selected-star"></i>
           <i className="fas fa-star star selected-star"></i>
@@ -38,13 +38,13 @@ export function FeaturedProduct() {
           <div className="product-price">$80.00</div>
         </div>
         <div className="product-card">
-          <a href="/styles/product/product.html" target="_blank">
+        
             <img
               src={kookaburraBall}
               className="card-img"
               alt="kookaburra-ball"
             />
-          </a>
+          
           <div className="card-info">Kookaburra Cricket Ball</div>
           <i className="fas fa-star star selected-star"></i>
           <i className="fas fa-star star selected-star"></i>
@@ -54,9 +54,9 @@ export function FeaturedProduct() {
           <div className="product-price">$20.00</div>
         </div>
         <div className="product-card">
-          <a href="/styles/product/product.html" target="_blank">
+        
             <img src={nikeShoes} className="card-img" alt="nike=shoes" />
-          </a>
+          
           <div className="card-info">Nike Sport Shoes</div>
           <i className="fas fa-star star selected-star"></i>
           <i className="fas fa-star star selected-star"></i>
@@ -67,9 +67,9 @@ export function FeaturedProduct() {
         </div>
 
         <div className="product-card">
-          <a href="/styles/product/product.html" target="_blank">
+        
             <img src={jersey} className="card-img" alt="nike=shoes" />
-          </a>
+          
           <div className="card-info">Cricket Jersey</div>
           <i className="fas fa-star star selected-star"></i>
           <i className="fas fa-star star selected-star"></i>
@@ -80,9 +80,9 @@ export function FeaturedProduct() {
         </div>
 
         <div className="product-card">
-          <a href="/styles/product/product.html" target="_blank">
+        
             <img src={nikeShoes} className="card-img" alt="nike=shoes" />
-          </a>
+          
           <div className="card-info">Nike Sport Shoes</div>
           <i className="fas fa-star star selected-star"></i>
           <i className="fas fa-star star selected-star"></i>
