9483e2f1a8beb2346cd24e5447f1a8549a972b3f
Samyak Shah <samyakshah3008@gmail.com>
2022-03-23 00:37:10 +0530

    FEAT: Product page component add

---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,21 @@
+import { Footer } from "../components/Footer";
+import { NavBar } from "../components/NavBar";
+
+import "./ProductPage.css";
+import { SideBar } from "../components/SideBar";
+import { Products } from "../components/Products";
+
+export function ProductPage() {
+  return (
+    <div>
+      <NavBar />
+
+      <div className="main-container">
+        <SideBar />
+        <Products />
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
