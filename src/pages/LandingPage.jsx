a9435eaac4e5429cf89b53573afe0d8dbbd61a93
Samyak Shah <samyakshah3008@gmail.com>
2022-03-22 12:25:39 +0530

    FEAT: Landing page component add

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,24 @@
+import { ExclusiveProduct } from "../components/ExclusiveProduct";
+import { FeaturedProduct } from "../components/FeaturedProduct";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import { NavBar } from "../components/NavBar";
+import { NewArrivals } from "../components/NewArrivals";
+import { Partners } from "../components/Partners";
+import { Testimonials } from "../components/Testimonials";
+import "./LandingPage.css"
+
+export function LandingPage(){
+    return(
+        <div>
+            <NavBar></NavBar>
+            <Header></Header>
+            <FeaturedProduct></FeaturedProduct>
+            <NewArrivals></NewArrivals>
+            <ExclusiveProduct></ExclusiveProduct>
+            <Testimonials></Testimonials>
+            <Partners></Partners>
+            <Footer></Footer>
+        </div>
+    )
+}
