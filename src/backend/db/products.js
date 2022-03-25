b731cf8f5eb68c5e13c9445b39adfb3b41586090
Samyak Shah <samyakshah3008@gmail.com>
2022-03-26 02:21:21 +0530

    FEAT: database updated

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -5,6 +5,12 @@ import nikeFootball from "../../assets/nike-football.png";
 import kookaburraBall from "../../assets/kookabura-ball.png";
 import jersey from "../../assets/jersey.png";
 import heroHeader from "../../assets/hero-header.png";
+import miExclusive from "../../assets/mi-exclusive.png";
+import spartanBat from "../../assets/spartan-bat.jpg";
+import nbBat from "../../assets/nb-bat.jpg";
+
+
+
 
 /**
  * Product Database can be added here.
@@ -18,9 +24,12 @@ export const products = [
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, repudiandae?",
     originalPrice: "5000",
-    price: "2999",
-    categoryName: "non-fiction",
+    price: 1000,
+    categoryName: "nike",
     percentOff: "53%",
+    inStock: false,
+    fastDelivery: false,
+    rating: 3
   },
   {
     _id: uuid(),
@@ -28,9 +37,12 @@ export const products = [
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, repudiandae?",
     originalPrice: "10000",
-    price: "2999",
-    categoryName: "horror",
+    price: 4000,
+    categoryName: "mrf",
     percentOff: "53%",
+    inStock: true,
+    fastDelivery: false,
+    rating: 4
   },
   {
     _id: uuid(),
@@ -38,9 +50,12 @@ export const products = [
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, repudiandae?",
     originalPrice: "1000",
-    price: "499",
-    categoryName: "fiction",
+    price: 1500,
+    categoryName: "nike",
     percentOff: "53%",
+    inStock: true,
+    fastDelivery: false,
+    rating: 2
   },
 
   {
@@ -48,29 +63,77 @@ export const products = [
     imageURL: jersey,
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, repudiandae?",
-    originalPrice: "5000",
-    price: "2999",
-    categoryName: "non-fiction",
+    originalPrice: "800",
+    price: 700,
+    categoryName: "kookabura",
     percentOff: "53%",
+    inStock: false,
+    fastDelivery: false,
+    rating: 1
   },
   {
     _id: uuid(),
     imageURL: kookaburraBall,
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, repudiandae?",
-    originalPrice: "10000",
-    price: "2999",
-    categoryName: "horror",
+    originalPrice: "799",
+    price: 1099,
+    categoryName: "kookabura",
     percentOff: "53%",
+    inStock: true,
+    fastDelivery: true,
+    rating: 3
   },
   {
     _id: uuid(),
     imageURL: heroHeader,
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, repudiandae?",
-    originalPrice: "1000",
-    price: "499",
+    originalPrice: "4000",
+    price: 3000,
     categoryName: "fiction",
     percentOff: "53%",
+    inStock: true,
+    fastDelivery: true,
+    rating: 3
+  },
+  {
+    _id: uuid(),
+    imageURL: miExclusive,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, repudiandae?",
+    originalPrice: "5500",
+    price: 3500,
+    categoryName: "fiction",
+    percentOff: "53%",
+    inStock: true,
+    fastDelivery: true,
+    rating: 5
+  }, 
+
+  {
+    _id: uuid(),
+    imageURL: nbBat,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, repudiandae?",
+    originalPrice: "10000",
+    price: 2999,
+    categoryName: "fiction",
+    percentOff: "53%",
+    inStock: true,
+    fastDelivery: false,
+    rating: 3
+  },{
+    _id: uuid(),
+    imageURL: spartanBat,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, repudiandae?",
+    originalPrice: "5000",
+    price: 4000,
+    categoryName: "fiction",
+    percentOff: "53%",
+    inStock: true,
+    fastDelivery: false,
+    rating: 5
   },
 ];
