5b41be0317292ec34b40d15f1c7c7c16d7af9fd1
Samyak Shah <samyakshah3008@gmail.com>
2022-03-26 02:22:36 +0530

    FEAT: function called for different filters and map update

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,14 +6,40 @@ import jersey from "../assets/jersey.png";
 import heroHeader from "../assets/hero-header.png";
 import Card from "./Card";
 import { useProduct } from "../contexts/product-context";
+import { useReducer } from "react";
+import { useFilter } from "../contexts/filtercontext";
+import { fastDeliveryFilter, priceRangeFilter, stockFilter, sortData } from "../utilities/filter-utils";
 
-export function Products() {
-  const { state } = useProduct();
 
+export function Products() {     
+
+
+  // const priceRangeList = priceRangeFilter(state.products, )
+
+
+  const { products } = useProduct();
+
+  const {state, dispatch} = useFilter()
+
+  const priceRangeList = priceRangeFilter(products, state.range);
+  const stockFilterList = stockFilter(priceRangeList, state.onlyInStock);
+  const fastDeliveryList = fastDeliveryFilter(
+    stockFilterList,
+    state.fastDelivery
+  );
+  
+  const sortFinalList = sortData(fastDeliveryList, state.sortBy);
+
+
+
+
+
+
+ 
   return (
     <div>
       <div className="product-display-container">
-        {state.products.map((item) => (
+        {fastDeliveryList.map((item) => (
           <Card key={item.id} item={item} />
         ))}
 
