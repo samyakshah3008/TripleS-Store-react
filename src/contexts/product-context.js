caf6b2b4fd6d94092215eec983933d8b22308831
Samyak Shah <samyakshah3008@gmail.com>
2022-03-26 02:23:31 +0530

    context for product update

--- src/contexts/product-context.js
+++ src/contexts/product-context.js
@@ -1,29 +1,27 @@
-import { useEffect, useReducer, createContext, useContext } from "react";
+import { useState, useEffect, useReducer, createContext, useContext } from "react";
 import axios from "axios";
 import { productReducer } from "../reducers/product-reducer";
 
-const initialValue = {
-  products: [],
-};
+
 
 const productContext = createContext();
 
-const ProductProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(productReducer, initialValue);
+const ProductProvider = ({ children }) => { 
+
+  const [products, setProducts] = useState([])
+
   useEffect(() => {
     (async () => {
       const productResponse = await axios.get("/api/products");
       console.log(productResponse.data.products);
       if (productResponse.status === 200) {
-        dispatch({
-          type: "INITIAL_PRODUCT",
-          payload: productResponse.data.products,
-        });
+       
+        setProducts(productResponse.data.products)
       }
     })();
   }, []);
   return (
-    <productContext.Provider value={{ state, dispatch }}>
+    <productContext.Provider value={{ products }}>
       {children}
     </productContext.Provider>
   );
