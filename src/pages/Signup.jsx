0ddeb7034c49905ac9be37c4ff23ed517a19e341
Samyak Shah <samyakshah3008@gmail.com>
2022-03-28 22:13:37 +0530

    signup page component add

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,44 @@
 import React from 'react'
 import { NavBar } from '../components/NavBar'
+import { useAuth } from '../contexts/auth-context'
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios'
+
+
+
 
 export default function Signup() {
+
+    const [userSignup, setUserSignup] = useState({email: "", password: "", firstName: "", lastName: ""})
+
+    const {setUser} = useAuth()     
+
+    const changeHandlerSignup = (e) => {
+        setUserSignup((previousState) =>  ({...previousState, [e.target.name]: e.target.value}))
+
+
+    }
+
+    // console.log(userSignup)
+
+    const signupUserHandler = async () =>  {
+        try {
+        const response = await axios.post("/api/auth/signup",userSignup)
+            console.log(response)
+            setUser({users: response.data.createdUser, token: response.data.encodedToken })
+            // navigate("/");
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    const  navigate = useNavigate();
+
+
+
   return (
         <>
 
@@ -14,34 +51,31 @@ export default function Signup() {
             <div class="login-signup-header">Sign up</div>
             <div class="name-container">
                 <div>Email</div>
-                <input class="login-signup-input" type="text" placeholder="Enter mail"/>
+                <input name='email' onChange={(e) => changeHandlerSignup(e)} value = {userSignup.email} class="login-signup-input" type="text" placeholder="Enter mail"/>
             </div> 
 
             <div class="name-container">
                 <div>Enter first name</div>
-                <input class="login-signup-input" type="text" placeholder="Enter first name"/>
+                <input name='firstName' onChange={(e) => changeHandlerSignup(e)} value = {userSignup.firstName} class="login-signup-input" type="text" placeholder="Enter first name"/>
             </div> 
 
             <div class="name-container">
                 <div>Enter last name</div>
-                <input class="login-signup-input" type="text" placeholder="Enter last name"/>
+                <input name='lastName' onChange={(e) => changeHandlerSignup(e)} value = {userSignup.lastName} class="login-signup-input" type="text" placeholder="Enter last name"/>
             </div> 
 
             <div class="password-container">
                 <div>Password</div>
-                <input class="login-signup-input" type="password" placeholder="Enter password"/>
+                <input name='password' onChange={(e) => changeHandlerSignup(e)} value = {userSignup.password} class="login-signup-input" type="password" placeholder="Enter password"/>
             </div> 
 
-            <div class="password-container">
-                <div>Confirm Password</div>
-                <input class="login-signup-input" type="password" placeholder="Confirm password"/>
-            </div> 
+            
 
             <div class="remember-me-container">
                 <div><input class="checkbox" type="checkbox"/></div>
                 <div class="checkbox-message">Accept all terms and conditions</div>
             </div> 
-            <button class="signup-btn btn2">Create account</button>
+            <button onClick={signupUserHandler} class="signup-btn btn2">Create account</button>
             <div class="login-instead-container">
             <a href="/styles/authentication/login.html"><div class="login-message">Already have account?</div></a> 
             <div><i class=" fas fa-chevron-right site-icon "></i></div>
