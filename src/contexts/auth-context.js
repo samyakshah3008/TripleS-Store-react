2a38c19374606d2db481adec8ffc68d18a520156
Samyak Shah <samyakshah3008@gmail.com>
2022-03-28 17:43:06 +0530

    auth context done

---
+++ src/contexts/auth-context.js
@@ -0,0 +1,17 @@
+import { createContext, useContext, useState} from "react";
+
+
+const createAuth = createContext({users: null, token: null})
+
+export const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState({users: null, token: null})
+
+    return(
+        <createAuth.Provider value={{user, setUser}}>
+            {children}
+        </createAuth.Provider>
+    )
+}
+
+export const useAuth = () => useContext(createAuth)
