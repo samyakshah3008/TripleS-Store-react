0ed91bb2f5a43da0877764112d2f3ae8baad3324
Samyak Shah <samyakshah3008@gmail.com>
2022-03-30 15:31:06 +0530

    wishlist page add

--- src/pages/WishlistPage.jsx
+++ src/pages/WishlistPage.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Card from '../components/Card';
-import { Footer } from '../components/Footer';
 import { NavBar } from "../components/NavBar";
 import { useWishlist } from '../contexts/wishlist-context';
 import "./wishlistpage.css"
+
 
 
 export default function WishlistPage() {
 
   const {wishlist} = useWishlist()
 
+  const wishlistid = wishlist.wishlist.map((item) => item._id)
 
   return (
     <div>
@@ -20,10 +21,10 @@ export default function WishlistPage() {
      <main> 
 
         <h1 className="product-type-titles">Your Wishlist</h1> 
-
-        <div className='empty-wishlist' >Your Wishlist is Empty</div>
+        
+        <div className='empty-wishlist' > {wishlist.wishlist.length === 0 ? "Your Wishlist is Empty" : "" } </div>
         <div className="product-display-container">
-        {wishlist.wishlist.map(item => <Card key={item.id} item={item} />) }
+        {wishlist.wishlist.map(item => <Card  inWishlist = { wishlistid.includes(item._id) } key={item.id} item={item} />) }
         </div>
         </main>
 
