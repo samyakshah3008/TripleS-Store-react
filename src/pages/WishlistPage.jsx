f82aea4c06e9a5c9599b6de0203e774edbc9f8ff
Samyak Shah <samyakshah3008@gmail.com>
2022-03-30 15:32:23 +0530

    REFACTOR: made code clean

--- src/pages/WishlistPage.jsx
+++ src/pages/WishlistPage.jsx
@@ -1,34 +1,35 @@
-import React from 'react'
-import Card from '../components/Card';
+import React from "react";
+import Card from "../components/Card";
 import { NavBar } from "../components/NavBar";
-import { useWishlist } from '../contexts/wishlist-context';
-import "./wishlistpage.css"
-
-
+import { useWishlist } from "../contexts/wishlist-context";
+import "./wishlistpage.css";
 
 export default function WishlistPage() {
+  const { wishlist } = useWishlist();
 
-  const {wishlist} = useWishlist()
-
-  const wishlistid = wishlist.wishlist.map((item) => item._id)
+  const wishlistid = wishlist.wishlist.map((item) => item._id);
 
   return (
     <div>
+      <NavBar />
 
-    <NavBar/>
-    
-    
-     <main> 
+      <main>
+        <h1 className="product-type-titles">Your Wishlist</h1>
 
-        <h1 className="product-type-titles">Your Wishlist</h1> 
-        
-        <div className='empty-wishlist' > {wishlist.wishlist.length === 0 ? "Your Wishlist is Empty" : "" } </div>
-        <div className="product-display-container">
-        {wishlist.wishlist.map(item => <Card  inWishlist = { wishlistid.includes(item._id) } key={item.id} item={item} />) }
+        <div className="empty-wishlist">
+          {" "}
+          {wishlist.wishlist.length === 0 ? "Your Wishlist is Empty" : ""}{" "}
         </div>
-        </main>
-
-   
+        <div className="product-display-container">
+          {wishlist.wishlist.map((item) => (
+            <Card
+              inWishlist={wishlistid.includes(item._id)}
+              key={item.id}
+              item={item}
+            />
+          ))}
+        </div>
+      </main>
     </div>
-  )
+  );
 }
