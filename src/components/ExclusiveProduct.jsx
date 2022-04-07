49ca99f74a38d8fa91835f480b03c7d3ad818e0b
Samyak Shah <samyakshah3008@gmail.com>
2022-04-07 18:10:43 +0530

    product list by category add

--- src/components/ExclusiveProduct.jsx
+++ src/components/ExclusiveProduct.jsx
@@ -1,7 +1,12 @@
 import miExclusive from "../assets/mi-exclusive.png"
+import { navigate, useNavigate } from "react-router-dom"
+import { useFilter } from "../contexts/filtercontext"
 
 
 export function ExclusiveProduct(){
+    const navigate = useNavigate()
+    const {dispatch} = useFilter()
+  
     return(
         <div>
               <div className="exclusive-product-container">
@@ -10,7 +15,8 @@ export function ExclusiveProduct(){
             <div className="exclusive-product-info">Exclusively Available on TripleS Store</div>
             <h1 className="exclusive-product-heading">Smart Band 4</h1>
             <p className="exclusive-product-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, reprehenderit.</p> 
-            <button className="shop-now-secondary-btn">Shop now</button>
+            <button onClick={() => {dispatch({ type: "EXCLUSIVE" })
+        navigate("/products")}} className="shop-now-secondary-btn">Shop now</button>
 
         </div>
         </div> 
